a000052ebbb121c3702229804ef39a8479c34c90
igregorioneto <igregoriodoneto@outlook.com>
2024-09-24 10:12:15 -0300

    feat: declaracao de variaveis

---
+++ study/2/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Joao"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "Y"
+	println(a)
+}
